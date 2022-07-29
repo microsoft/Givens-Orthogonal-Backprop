f9e3a75b67a046ee3a034ff6b478374f47132d11
can goksen <cagoksen@microsoft.com>
2022-07-29 10:19:14 +0000

    still hacky but less indented tests

--- rotMat/testCudaGivens.py
+++ rotMat/testCudaGivens.py
@@ -13,130 +13,140 @@ dtype = torch.float32
 
 # To have same results
 torch.manual_seed(10)
-
 dispResults = False
-for XisId in [True, False]:
-    for N in [15,33]:
-        for M in [N,N -10]:
-            K = N-M
-            rotUnit = GivensRotations.RotMat(N, M).to(device)
-            rotUnit.thetas.data = torch.ones(rotUnit.thetas.data.size()).to(device) * np.pi * 2
-            thetas = rotUnit.thetas.detach().to(torch.device('cpu')).requires_grad_(True)
-            
-            for batch in [N, 3, 32]:
-                G = torch.randn(N,batch).to(dtype).to(device)
 
-                if XisId:
-                    X = torch.eye(N, batch).to(dtype).to(device)
-                else:
-                    X = torch.randn(N, batch).to(dtype).to(device)
-                
-                UPyTorch = torch.clone(X).to('cpu')
+parameters = [
+    [15, 15, 15],
+    [15, 15, 3],
+    [15, 15, 32],
+    [15, 5, 15],
+    [15, 5, 3],
+    [15, 5, 32],
+    [33, 15, 15],
+    [33, 15, 3],
+    [33, 15, 32],
+    [33, 5, 15],
+    [33, 5, 3],
+    [33, 5, 32]
+    ]
+parameters = [[x] + param for x in [True,False] for param in parameters]
 
-                startFwd = torch.cuda.Event(enable_timing=True)
-                endFwd = torch.cuda.Event(enable_timing=True)
-                startFwd.record()
-                Ucustom = rotUnit.forward(X)
-                endFwd.record()
-                # Waits for everything to finish running
-                torch.cuda.synchronize()
 
-                #print(G.size(), Ucustom.size())
-                loss = torch.sum(G*Ucustom)
+for XisId, N, M, batch in parameters: 
+    K = N-M
+    rotUnit = GivensRotations.RotMat(N, M).to(device)
+    rotUnit.thetas.data = torch.ones(rotUnit.thetas.data.size()).to(device) * np.pi * 2
+    thetas = rotUnit.thetas.detach().to(torch.device('cpu')).requires_grad_(True)
 
-                startBck = torch.cuda.Event(enable_timing=True)
-                endBck = torch.cuda.Event(enable_timing=True)
-                startBck.record()
-                loss.backward()
-                endBck.record()
-                # Waits for everything to finish running
-                torch.cuda.synchronize()
+    G = torch.randn(N,batch).to(dtype).to(device)
 
-                # Milliseconds
-                forwardMilliseconds = startFwd.elapsed_time(endFwd)
-                backwardMilliseconds = startBck.elapsed_time(endBck)
-                if dispResults:
-                    print( '(In ms) Forward time: {0:.3f} | Backward time: {1:.3f}'.format(forwardMilliseconds,backwardMilliseconds) ) # milliseconds
-                gradCustom = rotUnit.thetas.grad.to(torch.device('cpu'))
-                Ucustom = Ucustom.to(torch.device('cpu'))
+    if XisId:
+        X = torch.eye(N, batch).to(dtype).to(device)
+    else:
+        X = torch.randn(N, batch).to(dtype).to(device)
+    
+    UPyTorch = torch.clone(X).to('cpu')
 
-                if dispResults:
-                    print("Custom result:")
-                    print("U:")
-                    print(Ucustom)
-                    print("thetaGrad:")
-                    print(gradCustom)
+    startFwd = torch.cuda.Event(enable_timing=True)
+    endFwd = torch.cuda.Event(enable_timing=True)
+    startFwd.record()
+    Ucustom = rotUnit.forward(X)
+    endFwd.record()
+    # Waits for everything to finish running
+    torch.cuda.synchronize()
 
-                # (i,j) with indices larger than dMax are left out
-                dMax = N-1
-                if M < N-1:
-                    dMax -= K
+    #print(G.size(), Ucustom.size())
+    loss = torch.sum(G*Ucustom)
 
-                Ntilde = N
-                deadNode = -1
-                if N%2 == 1:
-                    Ntilde += 1
-                    deadNode = Ntilde-1
+    startBck = torch.cuda.Event(enable_timing=True)
+    endBck = torch.cuda.Event(enable_timing=True)
+    startBck.record()
+    loss.backward()
+    endBck.record()
+    # Waits for everything to finish running
+    torch.cuda.synchronize()
 
-                tstart = time.time()
-                E = list()
-                for step in range(Ntilde-2,-1,-1):
-                    for blockIndx in range(int(Ntilde/2)):
+    # Milliseconds
+    forwardMilliseconds = startFwd.elapsed_time(endFwd)
+    backwardMilliseconds = startBck.elapsed_time(endBck)
+    if dispResults:
+        print( '(In ms) Forward time: {0:.3f} | Backward time: {1:.3f}'.format(forwardMilliseconds,backwardMilliseconds) ) # milliseconds
+    gradCustom = rotUnit.thetas.grad.to(torch.device('cpu'))
+    Ucustom = Ucustom.to(torch.device('cpu'))
 
-                        if blockIndx == 0:
-                            i = 0
-                        else:
-                            i = (blockIndx-1+step)%(Ntilde-1)+1
-                        j = ( (Ntilde-1)-blockIndx-1+step )%(Ntilde-1) + 1
-                        if i > j:
-                            temp = i
-                            i = j
-                            j = temp
+    if dispResults:
+        print("Custom result:")
+        print("U:")
+        print(Ucustom)
+        print("thetaGrad:")
+        print(gradCustom)
 
-                        if (i > dMax and j > dMax) or j==deadNode:
-                            continue
+    # (i,j) with indices larger than dMax are left out
+    dMax = N-1
+    if M < N-1:
+        dMax -= K
 
-                        E.append( (i,j) )
-                        thetaIndx = int( i*N - (i+2)*(i+1)/2 + j )
-                        cij = torch.cos(thetas[thetaIndx])
-                        sij = torch.sin(thetas[thetaIndx])
+    Ntilde = N
+    deadNode = -1
+    if N%2 == 1:
+        Ntilde += 1
+        deadNode = Ntilde-1
 
-                        GivMat = torch.eye(N)
-                        GivMat[i,i] = cij
-                        GivMat[j,j] = cij
-                        GivMat[i,j] = -sij
-                        GivMat[j,i] = sij
-                        UPyTorch = GivMat.matmul(UPyTorch)
+    tstart = time.time()
+    E = list()
+    for step in range(Ntilde-2,-1,-1):
+        for blockIndx in range(int(Ntilde/2)):
 
-                G = G.to(torch.device('cpu'))
-                loss = (G*UPyTorch).sum()
-                loss.backward()
-                if dispResults:
-                    print(" (in seconds) torch time: "+str(time.time()-tstart))
+            if blockIndx == 0:
+                i = 0
+            else:
+                i = (blockIndx-1+step)%(Ntilde-1)+1
+            j = ( (Ntilde-1)-blockIndx-1+step )%(Ntilde-1) + 1
+            if i > j:
+                temp = i
+                i = j
+                j = temp
 
-                if dispResults:
-                    print("Torch autodiff result:")
-                    print("U:")
-                    print(UPyTorch)
-                    print("thetaGrad:")
-                    print(thetas.grad)
+            if (i > dMax and j > dMax) or j==deadNode:
+                continue
 
-                if dispResults:
-                    print("\n\nComparison of custom and autodiff:\n---------------------------------")
-                    print("Max abs deviation of forwards: ")
-                    print(torch.absolute(Ucustom-UPyTorch).max())
-                    print("Max abs deviation of grads: ")
-                    print(torch.absolute(thetas.grad-gradCustom).max())
+            E.append( (i,j) )
+            thetaIndx = int( i*N - (i+2)*(i+1)/2 + j )
+            cij = torch.cos(thetas[thetaIndx])
+            sij = torch.sin(thetas[thetaIndx])
 
-                msgInfo = "N: " + str(N) + " M: " + str(M) + " batch: " + str(batch) + " XisId: " + str(XisId)
-                torch.testing.assert_close(Ucustom, UPyTorch, check_layout=True, msg= msgInfo)
-                torch.testing.assert_close(thetas.grad, gradCustom, check_layout=True, msg=msgInfo)
-                if N == batch and XisId:
-                    torch.testing.assert_close(Ucustom @ Ucustom.t(), torch.eye(N,N), msg=msgInfo)
-                torch.cuda.synchronize()
-                #print("Test passed for", msgInfo)
+            GivMat = torch.eye(N)
+            GivMat[i,i] = cij
+            GivMat[j,j] = cij
+            GivMat[i,j] = -sij
+            GivMat[j,i] = sij
+            UPyTorch = GivMat.matmul(UPyTorch)
+
+    G = G.to(torch.device('cpu'))
+    loss = (G*UPyTorch).sum()
+    loss.backward()
+    if dispResults:
+        print(" (in seconds) torch time: "+str(time.time()-tstart))
+
+    if dispResults:
+        print("Torch autodiff result:")
+        print("U:")
+        print(UPyTorch)
+        print("thetaGrad:")
+        print(thetas.grad)
+
+    if dispResults:
+        print("\n\nComparison of custom and autodiff:\n---------------------------------")
+        print("Max abs deviation of forwards: ")
+        print(torch.absolute(Ucustom-UPyTorch).max())
+        print("Max abs deviation of grads: ")
+        print(torch.absolute(thetas.grad-gradCustom).max())
+
+    msgInfo = "N: " + str(N) + " M: " + str(M) + " batch: " + str(batch) + " XisId: " + str(XisId)
+    torch.testing.assert_close(Ucustom, UPyTorch, check_layout=True, msg= msgInfo)
+    torch.testing.assert_close(thetas.grad, gradCustom, check_layout=True, msg=msgInfo)
+    if N == batch and XisId:
+        torch.testing.assert_close(Ucustom @ Ucustom.t(), torch.eye(N,N), msg=msgInfo)
+    torch.cuda.synchronize()
 
 print("All tests passed!")
-
-
-
--- rotMat/testCudaGivensDeep.py
+++ rotMat/testCudaGivensDeep.py
@@ -13,110 +13,122 @@ dtype = torch.float32
 
 # To have same results
 torch.manual_seed(10)
-
 dispResults = False
-for XisId in [True, False]:
-    for N in [15,33]:
-        for M in [N, N -10]:
-            K = N-M
-            rotUnit = GivensRotations.RotMat(N, M).to(device)
-            rotUnit.thetas.data = torch.ones(rotUnit.thetas.data.size()).to(device) * np.pi * 2
-            
-            for batch in [N, 3, 32]:
-                G = torch.randn(N,batch).to(dtype).to(device)
 
-                if XisId:
-                    X = torch.autograd.Variable(torch.eye(N, batch).to(dtype).to(device), requires_grad=True)
-                else:
-                    X = torch.autograd.Variable(torch.randn(N, batch).to(dtype).to(device), requires_grad=True)
-                
-                XExplicit = torch.autograd.Variable(torch.clone(X), requires_grad=True)
-                GExplicit = torch.clone(G)
+parameters = [
+    [15, 15, 15],
+    [15, 15, 3],
+    [15, 15, 32],
+    [15, 5, 15],
+    [15, 5, 3],
+    [15, 5, 32],
+    [33, 15, 15],
+    [33, 15, 3],
+    [33, 15, 32],
+    [33, 5, 15],
+    [33, 5, 3],
+    [33, 5, 32]
+    ]
+parameters = [[x] + param for x in [True,False] for param in parameters]
 
-                # START WITH IMPLICIT U CALCULATION
-                startFwd = torch.cuda.Event(enable_timing=True)
-                endFwd = torch.cuda.Event(enable_timing=True)
-                startFwd.record()
-                UXcustom = rotUnit.forward(X)
-                endFwd.record()
-                # Waits for everything to finish running
-                torch.cuda.synchronize()
 
-                loss = torch.sum(G*UXcustom)
+for XisId, N, M, batch in parameters: 
+    K = N-M
+    rotUnit = GivensRotations.RotMat(N, M).to(device)
+    rotUnit.thetas.data = torch.ones(rotUnit.thetas.data.size()).to(device) * np.pi * 2
+    G = torch.randn(N,batch).to(dtype).to(device)
 
-                startBck = torch.cuda.Event(enable_timing=True)
-                endBck = torch.cuda.Event(enable_timing=True)
-                startBck.record()
-                loss.backward()
-                endBck.record()
-                # Waits for everything to finish running
-                torch.cuda.synchronize()
+    if XisId:
+        X = torch.autograd.Variable(torch.eye(N, batch).to(dtype).to(device), requires_grad=True)
+    else:
+        X = torch.autograd.Variable(torch.randn(N, batch).to(dtype).to(device), requires_grad=True)
+    
+    XExplicit = torch.autograd.Variable(torch.clone(X), requires_grad=True)
+    GExplicit = torch.clone(G)
 
-                # Milliseconds
-                forwardMilliseconds = startFwd.elapsed_time(endFwd)
-                backwardMilliseconds = startBck.elapsed_time(endBck)
-                if dispResults:
-                    print( '(In ms) Forward time: {0:.3f} | Backward time: {1:.3f}'.format(forwardMilliseconds,backwardMilliseconds) ) # milliseconds
-                XgradCustom = X.grad.to(torch.device('cpu'))
-                UXcustom = UXcustom.to(torch.device('cpu'))
+    # START WITH IMPLICIT U CALCULATION
+    startFwd = torch.cuda.Event(enable_timing=True)
+    endFwd = torch.cuda.Event(enable_timing=True)
+    startFwd.record()
+    UXcustom = rotUnit.forward(X)
+    endFwd.record()
+    # Waits for everything to finish running
+    torch.cuda.synchronize()
 
-                if dispResults:
-                    print("Custom result:")
-                    print("UX:")
-                    print(UXcustom)
-                    print("XGrad:")
-                    print(XgradCustom)
+    loss = torch.sum(G*UXcustom)
 
-                # NOW FOR EXPLICIT U CALC
-                rotUnit.zero_grad()
-                startFwd = torch.cuda.Event(enable_timing=True)
-                endFwd = torch.cuda.Event(enable_timing=True)
-                startFwd.record()
-                UXExplicit = rotUnit.getU() @ XExplicit
-                endFwd.record()
-                # Waits for everything to finish running
-                torch.cuda.synchronize()
+    startBck = torch.cuda.Event(enable_timing=True)
+    endBck = torch.cuda.Event(enable_timing=True)
+    startBck.record()
+    loss.backward()
+    endBck.record()
+    # Waits for everything to finish running
+    torch.cuda.synchronize()
 
-                loss = torch.sum(GExplicit*UXExplicit)
+    # Milliseconds
+    forwardMilliseconds = startFwd.elapsed_time(endFwd)
+    backwardMilliseconds = startBck.elapsed_time(endBck)
+    if dispResults:
+        print( '(In ms) Forward time: {0:.3f} | Backward time: {1:.3f}'.format(forwardMilliseconds,backwardMilliseconds) ) # milliseconds
+    XgradCustom = X.grad.to(torch.device('cpu'))
+    UXcustom = UXcustom.to(torch.device('cpu'))
 
-                startBck = torch.cuda.Event(enable_timing=True)
-                endBck = torch.cuda.Event(enable_timing=True)
-                startBck.record()
-                loss.backward()
-                endBck.record()
-                # Waits for everything to finish running
-                torch.cuda.synchronize()
+    if dispResults:
+        print("Custom result:")
+        print("UX:")
+        print(UXcustom)
+        print("XGrad:")
+        print(XgradCustom)
 
-                # Milliseconds
-                forwardMilliseconds = startFwd.elapsed_time(endFwd)
-                backwardMilliseconds = startBck.elapsed_time(endBck)
-                if dispResults:
-                    print( '(In ms) Forward time: {0:.3f} | Backward time: {1:.3f}'.format(forwardMilliseconds,backwardMilliseconds) ) # milliseconds
-                XExplicitgradCustom = XExplicit.grad.to(torch.device('cpu'))
-                UXExplicit = UXExplicit.to(torch.device('cpu'))
+    # NOW FOR EXPLICIT U CALC
+    rotUnit.zero_grad()
+    startFwd = torch.cuda.Event(enable_timing=True)
+    endFwd = torch.cuda.Event(enable_timing=True)
+    startFwd.record()
+    UXExplicit = rotUnit.getU() @ XExplicit
+    endFwd.record()
+    # Waits for everything to finish running
+    torch.cuda.synchronize()
 
-                if dispResults:
-                    print("Explicit Custom result:")
-                    print("UX Explicit:")
-                    print(UXExplicit)
-                    print("XGrad Explicit:")
-                    print(XExplicitgradCustom)
+    loss = torch.sum(GExplicit*UXExplicit)
 
-                if dispResults:
-                    print("\n\nComparison of custom and autodiff:\n---------------------------------")
-                    print("Max abs deviation of forwards: ")
-                    print(torch.absolute(UXcustom-UXExplicit).max())
-                    print("Max abs deviation of grads: ")
-                    print(torch.absolute(XgradCustom-XExplicitgradCustom).max())
+    startBck = torch.cuda.Event(enable_timing=True)
+    endBck = torch.cuda.Event(enable_timing=True)
+    startBck.record()
+    loss.backward()
+    endBck.record()
+    # Waits for everything to finish running
+    torch.cuda.synchronize()
 
-                msgInfo = "N: " + str(N) + " M: " + str(M) + " batch: " + str(batch) + " XisId: " + str(XisId)
-                torch.testing.assert_close(UXcustom, UXExplicit, check_layout=True, msg= msgInfo)
-                torch.testing.assert_close(XgradCustom, XExplicitgradCustom, check_layout=True, msg=msgInfo)
-                if N == batch and XisId:
-                    torch.testing.assert_close(UXcustom @ UXcustom.t(), torch.eye(N,N), msg=msgInfo)
-                    torch.testing.assert_close(UXExplicit @ UXExplicit.t(), torch.eye(N,N), msg=msgInfo)
-                torch.cuda.synchronize()
-                #print("Test passed for", msgInfo)
+    # Milliseconds
+    forwardMilliseconds = startFwd.elapsed_time(endFwd)
+    backwardMilliseconds = startBck.elapsed_time(endBck)
+    if dispResults:
+        print( '(In ms) Forward time: {0:.3f} | Backward time: {1:.3f}'.format(forwardMilliseconds,backwardMilliseconds) ) # milliseconds
+    XExplicitgradCustom = XExplicit.grad.to(torch.device('cpu'))
+    UXExplicit = UXExplicit.to(torch.device('cpu'))
+
+    if dispResults:
+        print("Explicit Custom result:")
+        print("UX Explicit:")
+        print(UXExplicit)
+        print("XGrad Explicit:")
+        print(XExplicitgradCustom)
+
+    if dispResults:
+        print("\n\nComparison of custom and autodiff:\n---------------------------------")
+        print("Max abs deviation of forwards: ")
+        print(torch.absolute(UXcustom-UXExplicit).max())
+        print("Max abs deviation of grads: ")
+        print(torch.absolute(XgradCustom-XExplicitgradCustom).max())
+
+    msgInfo = "N: " + str(N) + " M: " + str(M) + " batch: " + str(batch) + " XisId: " + str(XisId)
+    torch.testing.assert_close(UXcustom, UXExplicit, check_layout=True, msg= msgInfo)
+    torch.testing.assert_close(XgradCustom, XExplicitgradCustom, check_layout=True, msg=msgInfo)
+    if N == batch and XisId:
+        torch.testing.assert_close(UXcustom @ UXcustom.t(), torch.eye(N,N), msg=msgInfo)
+        torch.testing.assert_close(UXExplicit @ UXExplicit.t(), torch.eye(N,N), msg=msgInfo)
+    torch.cuda.synchronize()
 
 print("All tests passed!")
 
