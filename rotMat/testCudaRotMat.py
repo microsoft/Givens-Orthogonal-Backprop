ab2da02f1d5baa8b610ffaaca9861c5e7e3a1035
can goksen <cagoksen@microsoft.com>
2023-04-04 20:47:15 +0000

    changed directory name

--- rotMat/testCudaRotMat.py
+++ rotMat/testCudaRotMat.py
@@ -14,7 +14,7 @@ N = 2000
 M = N
 K = N-M
 nThetas = int(N*(N-1)/2)
-batch_size =  1024
+batch_size =  32
 
 # To drop angle params we need to have at least the last *pair* of dimensions left out
 if K > 1:
@@ -25,15 +25,55 @@ print("N:",N, "| M:", M, "| #thetas:", nThetas, "| batch size:", batch_size,"\n"
 X = torch.randn(N,batch_size).to(dtype).to(device)
 G = torch.randn(X.size(0), X.size(1),requires_grad=True).to(dtype).to(device)
 thetas = torch.randn(nThetas,requires_grad=True).to(dtype).to(device)
-for teamRR in [True,  False]:
 
-    if teamRR: print("Using Team RR Scheduling:") 
-    else: print("Using Circle Method RR Scheduling:")
-    forward = rotMatFn.forwardTeamRR if teamRR else rotMatFn.forward
-    backward = rotMatFn.backwardTeamRR if teamRR else rotMatFn.backward
+'''linear = torch.randn(N,N).to(dtype).to(device)
+linear.requires_grad = True
+U = linear @ X  
+
+print("Using nnlinear and Autograd:")
+
+startFwd = torch.cuda.Event(enable_timing=True)
+endFwd = torch.cuda.Event(enable_timing=True)
+startFwd.record()
+U = linear @ X  
+torch.cuda.synchronize()
+endFwd.record()
+
+# Waits for everything to finish running
+torch.cuda.synchronize()
+
+# Report in milliseconds
+forwardMilliseconds = startFwd.elapsed_time(endFwd)
+print('Forward time: {0:.5f}'.format(forwardMilliseconds))
+    
+startBck = torch.cuda.Event(enable_timing=True)
+endBck = torch.cuda.Event(enable_timing=True)
+startBck.record()
+jvp = torch.autograd.backward( U, G )
+endBck.record()
+
+# Waits for everything to finish running
+torch.cuda.synchronize()
+
+# Report in milliseconds
+backwardMilliseconds = startBck.elapsed_time(endBck)
+print('Backward time: {0:.5f}'.format(backwardMilliseconds))
+print('Total time: {0:.5f}'.format(forwardMilliseconds+backwardMilliseconds), "\n")'''
+
+
+_ = rotMatFn.backwardTeamRR(thetas,rotMatFn.forwardTeamRR(X, thetas),G)
+torch.cuda.synchronize()
+
+for teamRR in [True]:
+    if teamRR: 
+        print("Using Team RR Scheduling:") 
+        forward, backward = rotMatFn.forwardTeamRR, rotMatFn.backwardTeamRR
+    else: 
+        print("Using Circle Scheduling:")
+        forward, backward = rotMatFn.forward, rotMatFn.backward
+
     # You *cannot* use time.time() to time cuda-enabled functions! The cpu
     # proceeds asynchronously, leading to ludicrous underestimates.
-
     # The elapsed_time function returns time in *milliseconds*
 
     startFwd = torch.cuda.Event(enable_timing=True)
@@ -41,6 +81,7 @@ for teamRR in [True,  False]:
     startFwd.record()
     U = forward(X,thetas)
     endFwd.record()
+    
     # Waits for everything to finish running
     torch.cuda.synchronize()
 
@@ -53,8 +94,8 @@ for teamRR in [True,  False]:
     startBck.record()
     jvp = backward(thetas,U,G)
     endBck.record()
-    # Waits for everything to finish running
     torch.cuda.synchronize()
+    # Waits for everything to finish running
 
     # Report in milliseconds
     backwardMilliseconds = startBck.elapsed_time(endBck)
